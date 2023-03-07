d00db3fbd4fec510af103c8208e239ae9f2218a1
Timothy Nunnink <46979634+tnunnink@users.noreply.github.com>
2023-03-07 08:12:15 -0600

    Create Contained.sql

---
+++ Contained.sql
@@ -0,0 +1,34 @@
+-- =============================================
+-- Author: Timothy Nunnink
+-- Create date: 11/8/2022
+-- Description:	Gets table of contained objects
+-- for the provided object id.
+-- =============================================
+ALTER FUNCTION [dbo].[Contained]
+(	
+	@ObjectId INT
+)
+RETURNS TABLE 
+AS
+RETURN 
+(
+	WITH CTE AS (
+	SELECT g.tag_name base_tag_name,
+			g.*,
+			0 as contained_level,
+			CAST('' AS NVARCHAR(329)) container_name,
+			CAST(g.tag_name as nvarchar(1000)) contained_path
+	FROM gobject g
+	WHERE gobject_id = @ObjectId 
+	UNION ALL
+	SELECT c.base_tag_name,
+			g.*,
+			c.contained_level + 1 as contained_level,
+			c.tag_name container_name,
+			CAST(CONCAT(c.contained_path, '.', g.contained_name) as nvarchar(1000)) contained_path
+	FROM gobject g
+		JOIN CTE c ON c.gobject_id = g.contained_by_gobject_id
+	)
+
+	SELECT * FROM CTE
+)
